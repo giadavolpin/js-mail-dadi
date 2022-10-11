814694558d5e14d060764545e0170be91fa5fc7d
giadavolpin <volpingiada@gmail.com>
2022-10-11 15:07:28 +0200

    inizio

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. 
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+/* 
+1. chiedere mail utente
+2. creare lista di accesso
+3. esito controllo positivo o negativo 
+
+1. random 1-6 giocatore
+2. random 1-6 computer
+3. vince punteggio più alto
+
+
+*/
