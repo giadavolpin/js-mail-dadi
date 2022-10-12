c1f5c725bdd66dfbcf6cec879fa892b07202c14b
giadavolpin <volpingiada@gmail.com>
2022-10-12 11:36:29 +0200

    dadi

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 
 /* 
 1. chiedere mail utente
-2. creare lista di accesso , sapere SE è nella lista,
-3. esito controllo positivo o negativo 
+2. creare lista di accesso , 
+3. SE è nella lista esito controllo positivo stampare esito
+3. SE non è nella lista esito controllo negativo stampare esito
 
 1. random 1-6 giocatore
 2. random 1-6 computer
@@ -16,17 +17,24 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 
 */
 
-/* const mail = input ("Qual'è la tua mail?")
+const email = document.getElementById('email');
+const btn = document.getElementById('btn');
+btn.addEventListener('click',
+    function(){
+        
 
-const listaDAccesso = [] 
-if (mail){
+    }
+    ) 
+
+const listaDAccesso = ['volpingiada@gmail.com', 'marco@gmail.com' , 'pippo@gmail.com' ] 
+if (email){
     listaDAccesso
 
-} */
+}
 
 
 
-let numeriGeneratiGiocatore = [];
+/* let numeriGeneratiGiocatore = [];
 let numeriGeneratiComputer = [];
 
 for (let i = 1 ; i <= 6 ; i++) {
@@ -34,6 +42,7 @@ for (let i = 1 ; i <= 6 ; i++) {
     numeriGeneratiGiocatore.push (numeroGiocatore);  
 }
 console.log(numeriGeneratiGiocatore);
+
 for (let i= 1 ; i <= 6; i++) {
     let numeroComputer = Math.floor(Math.random()* (6 - 1)) + 1;
     numeriGeneratiComputer.push (numeroComputer);
@@ -42,7 +51,18 @@ console.log(numeriGeneratiComputer);
 
 for (let i = 1 ; i <= numeriGeneratiGiocatore.length ; i++) {
     if(numeriGeneratiGiocatore[0] < numeriGeneratiComputer[0]){
-        console.log();
+        console.log(); NON SO CHE SCRIVERE QUI
     }
-}
+} */
 
+let numeroGiocatore = Math.floor(Math.random()* (6 - 1)) + 1;
+let numeroComputer = Math.floor(Math.random()* (6 - 1)) + 1;
+console.log(numeroGiocatore, numeroComputer);
+
+if (numeroGiocatore < numeroComputer){
+    console.log('hai perso');
+} else if (numeroGiocatore > numeroComputer) {
+    console.log('hai vinto')
+} else {
+    console.log('pareggio') 
+}
