dc594444183c38ca4c31919c370e55d7fa2350fd
giadavolpin <volpingiada@gmail.com>
2022-10-20 18:55:07 +0200

    ciclo

--- script.js
+++ script.js
@@ -13,20 +13,22 @@ const listaDAccesso = ['giada@gmail.com', 'mattia@gmail.com', 'pippo@gmail.com' 
 
 
 
-    let inviaMail = document.getElementById('btn').value;
-    btn.addEventListener('click', function(){
-        for (let i = 0; i < listaDAccesso.length; i++){
-            alert(mailUtente); 
-            if(mailUtente !== listaDAccesso[i]){
-                risposta.innerHTML = 'Email Non Presente';
-                /*  SE non è nella lista esito controllo negativo stampare esito */
-            } else {
-                risposta.innerHTML = 'Email Presente';
-                /*   SE è nella lista esito controllo positivo stampare esito */                 
-            }
-        } 
+let inviaMail = document.getElementById('btn').value;
+btn.addEventListener('click', function(){
+    for (let i = 0; i < listaDAccesso.length; i++){
+        let check = false;
+        if(mailUtente === listaDAccesso[i]){
+            check = true;
+            risposta.innerHTML = 'Email Presente';
+            /*   SE è nella lista esito controllo positivo stampare esito */   
+    } else {
+    risposta.innerHTML = 'Email Non Presente';
+    /*  SE non è nella lista esito controllo negativo stampare esito */              
+    }
+    } 
     }) ;
 
+    /* NON MI SEGNA SEMPRE SBAGLIATA LA MAIL  */
 
 
 
@@ -39,24 +41,24 @@ calcolare il punteggio più alto e quindi assegnare vincitore.
 */
 let numeriGeneratiGiocatore = [];
 let  numeriGeneratiComputer = [];
+//scrivo il ciclo per generare i 6 numeri
 for (let i= 0; i <= 6; i++){
-    let numeriGeneratiGiocatore = Math.floor(Math.random()* (6 - 1)) + 1;
-    numeriGeneratiGiocatore.push(numeriGeneratiGiocatore);  
+    let numeriGiocatore = Math.floor(Math.random()* (6 - 1)) + 1;
+    numeriGiocatore.push(numeriGeneratiGiocatore);  
 }
 /* non mi funziona il push */
+//scrivo il ciclo per generare gli altri 6 numeri
 for (let i= 0; i <= 6; i++){
-    let numeriGeneratiComputer = Math.floor(Math.random()* (6 - 1)) + 1;
-    numeriGeneratiComputer.push(numeriGeneratiComputer);
+    let numeriComputer = Math.floor(Math.random()* (6 - 1)) + 1;
+    numeriComputer.push(numeriGeneratiComputer);
 }
-
-
+//scrivo il ciclo per confrontare i due rispettivi array
 for (let i = 1 ; i <= numeriGeneratiGiocatore.length ; i++) {
     if(numeriGeneratiGiocatore[0] < numeriGeneratiComputer[0]){
         
     }
 } 
-
-console.log(numeroGiocatore, numeroComputer);
+console.log(numeriGeneratiGiocatore, numeriGeneratiComputer);
 
 if (numeriGeneratiGiocatore < numeriGeneratiComputer){
     console.log('hai perso');
