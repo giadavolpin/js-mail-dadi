3960cef0baf3830a8b97ed851c27c746a7a48a84
giadavolpin <volpingiada@gmail.com>
2022-10-20 18:24:51 +0200

    push non funziona dadi

--- script.js
+++ script.js
@@ -5,79 +5,63 @@ stampa un messaggio appropriato sull’esito del controllo.
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 
-/* 
-
-
-
-
-1. random 1-6 giocatore
-2. random 1-6 computer
-3. vince punteggio più alto
-
-
-*/
 /* 1. chiedere mail utente */
-const mailUtente = document.getElementById('emailUtente');
+const mailUtente = document.getElementById('emailUtente').value;
 let risposta = document.getElementById('risposta');
 /* 2. creare lista di accesso */
-const listaDAccesso = ['giada@gmail.com', 'mattia@gmail.com', 'pippo@gmail.com' ] 
+const listaDAccesso = ['giada@gmail.com', 'mattia@gmail.com', 'pippo@gmail.com' ];
+
 
 
     let inviaMail = document.getElementById('btn').value;
     btn.addEventListener('click', function(){
-        let check = false;
-        
-        for (let i = 0; i < listaDAccesso.lenght; i++){
-            if(mailUtente === listaDAccesso [i]){
-            check = true;
+        for (let i = 0; i < listaDAccesso.length; i++){
+            alert(mailUtente); 
+            if(mailUtente !== listaDAccesso[i]){
+                risposta.innerHTML = 'Email Non Presente';
+                /*  SE non è nella lista esito controllo negativo stampare esito */
+            } else {
+                risposta.innerHTML = 'Email Presente';
+                /*   SE è nella lista esito controllo positivo stampare esito */                 
             }
-            if(check) {
-               risposta.innerHTML = 'Email Presente';
-             /*   3. SE è nella lista esito controllo positivo stampare esito */
-            }
-            else {
-               risposta.innerHTML = 'Email Non Presente';
-               /* 4. SE non è nella lista esito controllo negativo stampare esito */
-        }
-    } 
-    }  
-    ) ;
+        } 
+    }) ;
 
 
 
 
-
-
-
-/* let numeriGeneratiGiocatore = [];
-let numeriGeneratiComputer = [];
-
-for (let i = 1 ; i <= 6 ; i++) {
-    let numeroGiocatore = Math.floor(Math.random()* (6 - 1)) + 1;
-    numeriGeneratiGiocatore.push (numeroGiocatore);  
+ 
+/* 
+creo array giocatore e array computer, 
+genero i num random x tutti e 2, 
+li metto dentro (push) agli rispettivi array 
+calcolare il punteggio più alto e quindi assegnare vincitore.
+*/
+let numeriGeneratiGiocatore = [];
+let  numeriGeneratiComputer = [];
+for (let i= 0; i <= 6; i++){
+    let numeriGeneratiGiocatore = Math.floor(Math.random()* (6 - 1)) + 1;
+    numeriGeneratiGiocatore.push(numeriGeneratiGiocatore);  
 }
-console.log(numeriGeneratiGiocatore);
-
-for (let i= 1 ; i <= 6; i++) {
-    let numeroComputer = Math.floor(Math.random()* (6 - 1)) + 1;
-    numeriGeneratiComputer.push (numeroComputer);
+/* non mi funziona il push */
+for (let i= 0; i <= 6; i++){
+    let numeriGeneratiComputer = Math.floor(Math.random()* (6 - 1)) + 1;
+    numeriGeneratiComputer.push(numeriGeneratiComputer);
 }
-console.log(numeriGeneratiComputer);
+
 
 for (let i = 1 ; i <= numeriGeneratiGiocatore.length ; i++) {
     if(numeriGeneratiGiocatore[0] < numeriGeneratiComputer[0]){
-        console.log(); NON SO CHE SCRIVERE QUI
+        
     }
-} */
-/* 
-let numeroGiocatore = Math.floor(Math.random()* (6 - 1)) + 1;
-let numeroComputer = Math.floor(Math.random()* (6 - 1)) + 1;
+} 
+
 console.log(numeroGiocatore, numeroComputer);
 
-if (numeroGiocatore < numeroComputer){
+if (numeriGeneratiGiocatore < numeriGeneratiComputer){
     console.log('hai perso');
-} else if (numeroGiocatore > numeroComputer) {
+} else if (numeriGeneratiGiocatore > numeriGeneratiComputer) {
     console.log('hai vinto')
 } else {
     console.log('pareggio') 
-} */
+} 
